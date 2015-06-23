27429379b8b95e327461d6a6a6b9413398f9ab1e
Nishant Verma <nishuverma@gmail.com>
2015-06-23 16:30:37 +0530

    minor fixes

--- src/test/java/scenarios/AndroidSetup.java
+++ src/test/java/scenarios/AndroidSetup.java
@@ -1,9 +1,8 @@
 package scenarios;
 
 
-import org.openqa.selenium.WebDriver;
+import io.appium.java_client.android.AndroidDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.io.File;
 import java.net.MalformedURLException;
@@ -11,7 +10,7 @@ import java.net.URL;
 
 
 public class AndroidSetup {
-    protected WebDriver driver;
+    protected AndroidDriver driver;
 
     protected void prepareAndroidForAppium() throws MalformedURLException {
         File appDir = new File("/Users/nishant/Development/AppiumDemo/apps");
@@ -25,6 +24,6 @@ public class AndroidSetup {
 
         //other caps
         capabilities.setCapability("app", app.getAbsolutePath());
-        driver = new RemoteWebDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+        driver =  new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
     }
 }
--- src/test/java/scenarios/AppiumTest.java
+++ src/test/java/scenarios/AppiumTest.java
@@ -1,19 +1,19 @@
 package scenarios;
 
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import pages.LandingPage;
 import pages.LoginPage;
 
 public class AppiumTest extends AndroidSetup {
 
-    @BeforeMethod
+    @BeforeClass
     public void setUp() throws Exception {
         prepareAndroidForAppium();
     }
 
-    @AfterMethod
+    @AfterClass
     public void tearDown() throws Exception {
         driver.quit();
     }
